295b406710881c2e8a259ea74842a6de3ed51c2c
CORREIA Lucie <lucie.correia@telecomnancy.eu>
2024-01-31 15:19:09 +0100

    ex5

---
+++ ex5.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+#include <unistd.h> // pour sleep
+
+// Les pointeurs en C
+void tp0(){
+    int age_utilisateur = 25;
+    printf("Age utilisateur: %d\n", age_utilisateur);
+    printf("Adresse: %p\n", &age_utilisateur);
+}
+
+void tp1(){
+    char c = 'A';
+    printf("information sur ma varibale\n");
+    printf("\ttype: char\n");
+    printf("\ttaille:%ld octets\n", sizeof(c)); 
+    printf("\tcontenu: %c\n", c);
+    printf("\tadresse: %p\n", &c); // l'adresse c'est %p
+}
+
+void tp2(){
+    int nombre = 0;
+    int* p_nombre = &nombre; // pointeur vers nombre
+    printf("donnez un nombre entier:");
+    scanf("%d", p_nombre);
+    printf("le nombre %d est stockée à l'adresse %p\n", nombre, p_nombre); // on peut mettre ", *p_nombre, p_nombre); aussi
+}
+
+int main(){
+    // tp0();
+    // tp1();
+    tp2();
+    return 0;
+}
