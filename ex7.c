d0516f30433a4e0bf59f283d89303efecfd62892
CORREIA Lucie <lucie.correia@telecomnancy.eu>
2024-02-19 22:52:44 +0100

    str

--- ex7.c
+++ ex7.c
@@ -6,14 +6,24 @@
 // Les chaines de char
 
 // transfromer un tableau de char en str:
-
-
-void tp0(){
+void tp1(){
     char chaine[] = {'c', 'o', 'u', 'c', 'o', 'u', '\0'};
-    
+    printf("- %s\n", chaine); // en effet on affiche que coucou car le \0 est le caractère de fin de chaine
+}
+
+// Carte ID
+void tp2(){
+    const int TAILLE = 100; // donc 99 char max car il y a /0
+    char date[TAILLE];
+    char nom[TAILLE];
+    char prenom[TAILLE];
+    printf("Entrez votre date de naissance au format jj/mm/aaaa, nom et prénom:");
+    scanf("%s %s %s", date, nom, prenom);
+    printf("Date de naissance : %s\nNom : %s\nPrénom : %s\n", date, nom, prenom);
 }
 
 int main(){
     tp1();
+    tp2();
     return 0;
 }
