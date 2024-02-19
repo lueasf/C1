9109e788c09457edf5656cc9dec9e5351f712341
CORREIA Lucie <lucie.correia@telecomnancy.eu>
2024-02-19 22:42:13 +0100

    strings

--- ex7.c
+++ ex7.c
@@ -4,3 +4,16 @@
 #include <unistd.h> // pour sleep
 
 // Les chaines de char
+
+// transfromer un tableau de char en str:
+
+
+void tp0(){
+    char chaine[] = {'c', 'o', 'u', 'c', 'o', 'u', '\0'};
+    
+}
+
+int main(){
+    tp1();
+    return 0;
+}
