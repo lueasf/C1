46439bcb1e4911767d5e8353b94d538f3a1f0ce8
CORREIA Lucie <lucie.correia@telecomnancy.eu>
2024-02-21 14:53:24 +0100

    majuscule

--- ex7.c
+++ ex7.c
@@ -22,8 +22,30 @@ void tp2(){
     printf("Date de naissance : %s\nNom : %s\nPrénom : %s\n", date, nom, prenom);
 }
 
+
+// Maj
+void tp3(){
+    char chaine[101];
+    int index = 0;
+    printf("Entrez une chaine de char de maximum 100 charactères:\n");
+    // pas de scanf car il s'arrête au premier espace
+    fgets(chaine, 100, stdin); // (tab ou on va stoquer la chaine, 
+    // entier = nb max de char à lire, source = stdin (lecture clavier)
+    if (fgets(chaine, 100, stdin) == NULL){
+        printf("Erreur de lecture\n");
+    }
+    while(chaine[index] != '\0' && index<100){
+        if (chaine[index] >= 'a' && chaine[index] <= 'z'){
+            chaine[index] = chaine[index] - 32;
+        }
+        index++;
+    }
+    printf("%s\n", chaine);
+}
+
 int main(){
     tp1();
     tp2();
+    tp3();
     return 0;
 }
