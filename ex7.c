27ad6adb126d9da3fe2a9ff0432c89ca2e0e2650
CORREIA Lucie <lucie.correia@telecomnancy.eu>
2024-02-26 10:47:50 +0100

    mdptp

--- ex7.c
+++ ex7.c
@@ -71,18 +71,46 @@ void tp5(){
     char mdp[TAILLE + 1];
     int taille_mdp = 0;
 
-    printf("donnez un mdp: \n");
-    if (fgets(mdp, 100, stdin) == NULL)
-        printf("Erreur de lecture\n");
-    taille_mdp = strlen(mdp) - 1; // on enlève le \n
-    mdp[taille_mdp] = '\0'; // on remplace le \n par \0
+    int valide = 0;
+    do
+    {
+        printf("donnez un mdp: \n");
+        if (fgets(mdp, 100, stdin) == NULL)
+            printf("Erreur de lecture\n");
+        taille_mdp = strlen(mdp) - 1; // on enlève le \n
+        mdp[taille_mdp] = '\0'; // on remplace le \n par \0
 
-    if (taille_mdp < MIN || taille_mdp > MAX)
-        printf("le mdp doit [5-10] char.\n");
-    
-    int maj = 0;
-    int special = 0;
-    int chiffre = 0;
+        if (taille_mdp < MIN || taille_mdp > MAX)
+            printf("le mdp doit [5-10] char.\n");
+            continue; // comme un break, arrete l'execution de la boucle courante
+        
+        int maj = 0;
+        int special = 0;
+        int chiffre = 0;
+
+        for (int i = 0; i<taille_mdp; i++){
+            if (mdp[i] >= 'A' && mdp[i] <= 'Z')
+                maj = 1;
+            else if (mdp[i] >= '0' && mdp[i] <= '9')
+                chiffre = 1;
+            else if (mdp[i] < 'a' || mdp[i] > 'z')
+                special = 1;    if (maj == 0)
+        printf("le mdp doit contenie au moins 1 majuscule.\n");
+        }
+
+        if (maj == 0)
+            printf("le mdp doit contenie au moins 1 majuscule.\n");
+            continue;
+
+        if (special == 0)
+            printf("le mdp doit contenie au moins 1 char spécial.\n");
+            continue;
+
+        if (chiffre == 0)
+            printf("le mdp doit contenie au moins 1 chiffre.\n");
+            continue;
+        valide == 1;
+}while(valide == 0); // tant que valide = 0, on continue la boucle while
 }
 
 int main(){
@@ -90,6 +118,5 @@ int main(){
     tp2();
     tp3();
     tp4();
-    tp5();
     return 0;
 }
---
+++ ex7_mdp.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+#include <unistd.h> // pour sleep
+#include <string.h> // pour les chaines de char
+
+
+//Création de mot de passe
+int main(){
+    const int TAILLE = 100;
+    const int MIN = 5;
+    const int MAX = 10;
+    char mdp[TAILLE + 1];
+    int taille_mdp = 0;
+
+    int valide = 0;
+    do
+    {
+        printf("donnez un mdp: \n");
+        if (fgets(mdp, 100, stdin) == NULL)
+            return -1;
+
+        taille_mdp = strlen(mdp) - 1; // on enlève le \n
+        mdp[taille_mdp] = '\0'; // on remplace le \n par \0
+
+        if (taille_mdp < MIN || taille_mdp > MAX){
+            printf("le mdp doit [5-10] char.\n");
+            continue; // comme un break, arrete l'execution de la boucle courante
+            }   
+        int maj = 0;
+        int special = 0;
+        int chiffre = 0;
+
+        for (int i = 0; i<taille_mdp; i++){
+            if (mdp[i] >= 'A' && mdp[i] <= 'Z')
+                maj = 1;
+            else if (mdp[i] >= '0' && mdp[i] <= '9')
+                chiffre = 1;
+            else if (mdp[i] < 'a' || mdp[i] > 'z')
+                special = 1; 
+        }
+
+        if (maj == 0){
+            printf("le mdp doit contenie au moins 1 majuscule.\n");
+            continue;
+        }
+
+        if (special == 0){
+            printf("le mdp doit contenie au moins 1 char spécial.\n");
+            continue;
+        }
+
+        if (chiffre == 0){
+            printf("le mdp doit contenie au moins 1 chiffre.\n");
+            continue;
+        }
+
+        valide = 1;
+    }while(valide == 0); // tant que valide = 0, on continue la boucle while
+    return 0;
+}
